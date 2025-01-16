f477a95b5f284173b3b29bb3c6aa7aabf5222345
Maley Venkatesh <venkateshmaley1@gmail.com>
2025-01-16 22:56:37 +0530

    contact us page

--- sreeviaai-frontend/src/app/app.routes.ts
+++ sreeviaai-frontend/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { AboutComponent } from './about/about.component';
 import { CareersComponent } from './careers/careers.component';
 import { ServicesComponent } from './services/services.component';
 import { NewsComponent } from './news/news.component';
+import { ConnectComponent } from './connect/connect.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent }, 
     { path: 'about', component: AboutComponent }, 
     { path: 'careers', component: CareersComponent }, 
     { path: 'services', component: ServicesComponent },
-    { path: 'news', component: NewsComponent }
+    { path: 'news', component: NewsComponent },
+    { path: 'connect', component: ConnectComponent}
 ];
